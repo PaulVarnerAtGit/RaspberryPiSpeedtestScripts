cc4a4198e3d5c24519aeb80647c4aa6130d8abfd
PaulVarnerAtGit <PaulVarner@outlook.com>
2020-12-29 16:56:00 -0500

    Initial load of roll_speedtest_log.sh script file

---
+++ roll_speedtest_log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Append nightly action file contents to speedtest_date-info.log file
+# This script is intended to be run at 11:59 pm each day 
+
+#get current  date and time
+today=`date +%m-%d-%Y_%H:%M:%S`
+
+# Create entry in roll_speedtest.log file - starting movement process.
+echo 'Started move of speedtest.log to log_history folder at $today' >> /home/pi/scripts/roll_speedtest.log
+
+# move current speedtest.log file to the log_history folder 
+mv /home/pi/scripts/speedtest.log /home/pi/log_hitory/speedtest_"$today".log 
+
+#get current  date and time
+today=`date +%m-%d-%Y_%H:%M:%S`
+
+# End entry placed in roll_speedtest.log file - ending movement process.
+echo 'Move of speedtest.log to log_history folder at $today completed!' >> /home/pi/scripts/roll_speedtest.log
+echo '' >> /home/pi/scripts/roll_speedtest.log

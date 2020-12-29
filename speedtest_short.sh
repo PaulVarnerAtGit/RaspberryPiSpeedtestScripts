4a4c2d1e71b1fdd79c15bd6f85b81f23fd2cb283
PaulVarnerAtGit <PaulVarner@outlook.com>
2020-12-29 17:23:22 -0500

    Version of speedtest.sh that outputs informaiton to the command line.

---
+++ speedtest_short.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# short script to output speedtest to the console.  
+
+# Start script date and time
+date
+
+# speedtest command
+speedtest-cli --simple
+
+# End script date and time
+date
